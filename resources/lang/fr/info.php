a068bc4eee3630a22ea347de1392e73e62f75482
silasmas <silasjmas@gmail.com>
2024-09-28 08:17:10 +0100

    ok

---
+++ resources/lang/fr/info.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @author Xanders
+ * @see https://www.linkedin.com/in/xanders-samoth-b2770737/
+ */
+
+return [
+    'view_more' => 'Voir plus',
+    'view_more2' => 'Tout voir',
+    'avantage' => [
+        'grandtitre' => 'Titre',
+        'stitre' => 'dire un mot ',
+        'section' => 'Pourquoi faire un don',
+        'sectionsponsort' => 'Nos sponsort evenementiel',
+        'btnevent' => 'Devenir sponsort',
+        's1' => [
+            't1' => 'Avantage 1',
+            'desc' => 'description',
+        ],
+        's2' => [
+            't1' => 'Avantage 2',
+            'desc' => 'description',
+        ],
+        's3' => [
+            't1' => 'Avantage 3',
+            'desc' => 'description',
+        ],
+        's4' => [
+            't1' => 'Avantage 4',
+            'desc' => 'description',
+        ],
+    ],
+    'event' => [
+        'grandtitre' => 'Titre de evenement',
+        'stitre' => 'dire un mot sur les evement',
+        'section' => 'Nos évenement recents',
+        'sectionsponsort' => 'Nos sponsort evenementiel',
+        'btnevent' => 'Devenir sponsort',
+    ],
+    'team' => [
+        'grandtitre' => 'Titre de l\'équipe',
+        'stitre' => 'dire un mot sur l\'équipe',
+        'section' => 'Voire notre équipe',
+    ],
+    'blog' => [
+        'titreBlog' => 'Blog & Article',
+        'stitreBlog' => 'Sous titre du blog',
+    ],
+    'temoignage' => [
+        'titreTemoignage' => 'Témoignages',
+        'gtitreTemoignage' => 'Titre en un mot',
+        'descTemoignage' => 'Courte descriptions sur les temoignages',
+    ],
+    'cause' => [
+        'section' => 'Ce qui nous motive',
+        'gtitre' => 'Grand titre',
+    ],
+    'slide' => [
+        's1' => [
+            't1' => 'Santé',
+            't2' => 'Santé pour tous',
+            't3' => 'La Santé communautaire',
+            'desc' => 'La santé aux milieux de la communauté pour faciliter l’accès à la santé de qualité, pour sauver la communauté contre les maladies;',
+        ],
+        's2' => [
+            't1' => 'Education',
+            't2' => 'Education et la formation professionnaire.',
+            't3' => 'L’éducation et la formation est la richesse de l’homme.',
+            'desc' => 'Éduquer en enfants c’est garantir son avenir et pour toute une nation et une génération.',
+        ],
+        's3' => [
+            't1' => 'Droits humains',
+            't2' => 'Le Respect de la personne.',
+            't3' => 'La parité entre la femme et l’homme.',
+            'desc' => 'Le droits de la femmes doivent être respecter comme toute personne utile dans la société.',
+        ],
+        's4' => [
+            't1' => 'Environnement',
+            't2' => 'Protéger nous la nature.',
+            't3' => 'Que tous contribue dans la protection de l’environnement.',
+            'desc' => 'La gestion climatique est très important et l’impliqué communautaire.',
+        ],
+        's5' => [
+            't1' => 'Agriculture',
+            't2' => 'Agriculture et la nutrition.',
+            't3' => 'Manger Bio chez vous.',
+            'desc' => 'Avec l’agriculture la communauté pourrais avoir une alimentation équilibrer et une augmentation des différente vitamine.',
+        ],
+    ],
+
+];
